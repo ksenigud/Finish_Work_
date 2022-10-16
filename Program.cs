827dd1484167266bc8e681941ba5a992d1ec7aa5
ksenigud <ms.kseniya0606@mail.ru>
2022-10-16 23:06:19 +0300

    метод счетчик

--- Program.cs
+++ Program.cs
@@ -4,3 +4,15 @@
     return Console.ReadLine().Split(" ");
 }
 
+int Count(string[] inArray) 
+{
+    int count = 0;
+    for(int i = 0; i < inArray.Length; i++) {
+        if(inArray[i].Length <= 3) 
+        {
+            count++;
+        }
+    }
+
+    return count;
+}
