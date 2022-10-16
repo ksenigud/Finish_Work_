0605a3b7ee0e75264f0ab4ad74d26417e27aed8d
ksenigud <ms.kseniya0606@mail.ru>
2022-10-16 23:08:01 +0300

    итговый массив

--- Program.cs
+++ Program.cs
@@ -16,3 +16,21 @@ int Count(string[] inArray)
 
     return count;
 }
+
+string[] OutputArray(string[] inArray) 
+{
+    string[] output = new string[Count(inArray)];
+    for(int i = 0, j = 0; i < inArray.Length; i++) 
+    {
+        if(inArray[i].Length <= 3) 
+        {
+            output[j] = inArray[i];
+            j++;
+        }
+    }
+
+    return output;
+}
+string[] inArray = InputArray();
+string[] outArray = OutputArray(inArray);
+Console.WriteLine($"3 or less symbols elements are: {string.Join(", ", outArray)}");
